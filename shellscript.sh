c55f403f39710897f8ddac79f5082ce22308c432
krushnaprakash <krushnalohar801@gmail.com>
2023-10-30 14:10:30 +0530

    day_1

---
+++ shellscript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#for shell scripting practice 
+
+<<comment
+Run using 
+./script.sh  #need execute permission
+/path/script.sh
+bash  script.sh
+comment
+#This is single line comment
+# >> for multi line comment
+#comment= this is short discription or summary
+
+#VAR_NAME=value
+#VAR_NAME=$(hostname)
+# for practice -----let start with variable________________
+age=23
+name=krushna
+city=pune
+echo "myself $name ,age is $age, i am currently living in $city"
+#for practice __________variable constant.
+#use_readonly
+readonly colleage=nmu
+echo "i have completed my graduation in bcs form $colleage jalgaon"
+
+

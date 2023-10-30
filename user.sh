751918c8a068b9469ab3d7bd74c2dcecb3209eb3
krushnaprakash <krushnalohar801@gmail.com>
2023-10-30 15:59:57 +0530

    4th_day

---
+++ user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+<<comment
+talking input from user 
+read <var_name>
+read -p "your name " NAME 
+comment
+
+#example
+read name 
+echo " this machine name is $name"
+####################################
+read -p "what is your name" name 
+echo "your name is $name "
+

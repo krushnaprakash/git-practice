bd9d1f176679337b3a9260bd764481ece368ec81
krushnaprakash <krushnalohar801@gmail.com>
2023-10-31 20:14:54 +0530

    case_pra.

---
+++ case.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#its a similar to switch case / better option to elif 
+<<case
+echo "hey choose an option"
+echo " a = to see the current date "
+echo " b = list all the files in current directory"
+read choice 
+case $choice in 
+         a) date ;;
+         b) ls ;;
+         *) echo "non a valid input"
+esac
+case
+echo " provide an option "
+echo "a for print date"
+echo "b for list of script"
+echo "c to check the current location "
+read choise
+case $choise in
+	a) date;;
+	b) ls;;
+	c) pwd;;
+	*) echo "please provide a valid value" 
+	#use other method 
+	#	a) 
+	#	echo " Todays date is :"
+	#	;;
+esac
+

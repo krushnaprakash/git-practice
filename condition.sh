eacbb03c709efb6b806a856f91970039e785694c
krushnaprakash <krushnalohar801@gmail.com>
2023-10-31 17:16:03 +0530

    conditon_var

---
+++ condition.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#conditional statement _____dision of conditonal bases:
+<<condition
+if [$marks -gt 40 ]  -gt = greter than 
+then 
+    echo "you are PASS "
+else
+    echo "you are FAIL "
+fi
+condition
+read -p "enter your mark:" marks
+if [[ $marks -gt 40 ]] 
+then 
+	echo " CONGRATULATION you are PASS"
+else 
+	echo "ohh shit ! you are FAIL !!!!!!!"
+fi 

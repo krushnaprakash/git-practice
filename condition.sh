2b6d3d427e984cfde2c5a1a16ec47bec58073fbb
krushnaprakash <krushnalohar801@gmail.com>
2023-10-31 18:10:08 +0530

    multi_condition

--- condition.sh
+++ condition.sh
@@ -17,3 +17,40 @@ then
 else 
 	echo "ohh shit ! you are FAIL !!!!!!!"
 fi 
+###############################multi condition add
+<<condition
+_______________________________________
+Equal                      -eql==
+________________________________________
+Greaterthanorequalto       -le 
+________________________________________
+Lessthanorequalto          -ne/!=
+________________________________________
+Greater than               -gt
+________________________________________
+Less than                  -lt
+_______________________________________
+
+if [ $marks -ge 80 ] 
+then 
+    echo "frist divison"
+elif [ $marks -ge 60 ]
+then 
+    echo "second division"
+else 
+    echo "fail "
+fi 
+condition
+#######################practice 
+read -p "please type your marks" marks
+if [[ $marks -ge 80 ]]
+then 
+	echo "you are pass in frist division "
+elif [[ $marks -ge 50 ]]
+then  
+	echo "you are pass in SECOND DIVISON"
+else 
+	echo "ohh no!!! sorry to say that you are fail"
+fi
+
+

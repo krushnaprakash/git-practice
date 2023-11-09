ec9b17ae4f1dd4f7d6425a34bbbec4367c357b80
krushnaprakash <krushnalohar801@gmail.com>
2023-11-09 13:23:53 +0530

    reset_mixed

--- bash.sh
+++ bash.sh
@@ -6,3 +6,9 @@ city=pune
 hometown=jalgaon
 echo " myself $name my cuurent age is $age i am currently living in $city city and iam belong to $hometown"
 
+
+#git reset mixed 
+name=krish
+age=23
+echo "my name is $name i am $age years old"
+
